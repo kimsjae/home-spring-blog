bf756d6a7b4f8b50034b7e86cc954fb47500fec5
kimseongjae <stwin755@gmail.com>
2024-02-18 17:13:46 +0900

    게시글 상세보기 구현 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -12,7 +13,7 @@ import java.util.List;
 public class BoardController {
     private final BoardRepository boardRepository;
 
-    @GetMapping({ "/", "/board" })
+    @GetMapping({ "/"})
     public String index(HttpServletRequest request) {
         List<Board> boardList = boardRepository.findAll();
         request.setAttribute("boardList", boardList);
@@ -20,13 +21,17 @@ public class BoardController {
         return "index";
     }
 
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable int id, HttpServletRequest request) {
+        BoardResponse.DetailDTO detailDTO = boardRepository.findById(id);
+        request.setAttribute("board", detailDTO);
+        return "board/detail";
+    }
+
     @GetMapping("/board/saveForm")
     public String saveForm() {
         return "board/saveForm";
     }
 
-    @GetMapping("/board/1")
-    public String detail() {
-        return "board/detail";
-    }
+
 }
--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,5 +17,27 @@ public class BoardRepository {
         Query query = em.createNativeQuery("select * from board_tb order by id desc", Board.class);
 
         return query.getResultList();
+    }
+
+    public BoardResponse.DetailDTO findById(int idx) {
+        Query query = em.createNativeQuery("select b.id, b.title, b.content, b.user_id, u.username from board_tb b inner join user_tb u on b.user_id = u.id where b.id = ?");
+        query.setParameter(1, idx);
+
+        Object[] row = (Object[]) query.getSingleResult();
+
+        Integer id = (Integer) row[0];
+        String title = (String) row[1];
+        String content = (String) row[2];
+        Integer userId = (Integer) row[3];
+        String username = (String) row[4];
+
+        BoardResponse.DetailDTO detailDTO = new BoardResponse.DetailDTO();
+        detailDTO.setId(id);
+        detailDTO.setTitle(title);
+        detailDTO.setContent(content);
+        detailDTO.setUserId(userId);
+        detailDTO.setUsername(username);
+
+        return detailDTO;
     }
 }
